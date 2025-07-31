f11f3b3ba4b065819cb06a91f8e05443f853911c
lruma113 <grace20270@gmail.com>
2025-08-01 02:00:05 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-
 export const metadata = {
   title: '拍賣網站',
-  description: 'LINE 社群專用小卡拍賣頁面',
+  description: 'LINE 社群導用卡拍賣首頁',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
